9c0922797e5c3de9fe3b11389782b624fb07d503
Mobarak Hossain <47492246+mobarak10@users.noreply.github.com>
2022-09-14 23:46:38 +0600

    solve problem number two

--- app/Http/Controllers/ProblemSolveController.php
+++ app/Http/Controllers/ProblemSolveController.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProblemSolveController extends Controller
 {
@@ -13,11 +14,15 @@ class ProblemSolveController extends Controller
      * @param $id
      * @return Application|Factory|View
      */
-    public function solveProblem($id): View|Factory|Application
+    public function solveProblem($id)
     {
         if ($id == 1) {
             $problem_one_data = $this->solveProblemOne();
             return view('problem-one', compact('problem_one_data'));
+        }
+        if ($id == 2) {
+            return $problem_two_data = $this->solveProblemTwo();
+            return view('problem-two', compact('problem_two_data'));
         }
     }
 
@@ -36,5 +41,29 @@ class ProblemSolveController extends Controller
         preg_match_all("/([^,= ]+)=([^,= ]+)/", $string, $newArray);
 
         return $newArray;
+    }
+
+    /**
+     * solve problem number two
+     */
+    public function solveProblemTwo(): array|int|string
+    {
+        $given_array = array('0'=>'z1', '1'=>'Z10', '2'=>'z12', '3'=>'Z2', '4'=>'z3');
+        // get the size of array
+        $count = count($given_array);
+
+        // initialize outer for loop
+        for ($i = 0; $i < $count; $i++) {
+            // initialize inner for loop
+            for ($j = $i + 1; $j < $count; $j++) {
+                // check index value of outer array is bigger than inner array index value
+                if (Str::of(strtolower($given_array[$i]))->after('z') > Str::of(strtolower($given_array[$j]))->after('z')) {
+                    $temp = $given_array[$i]; // value of outer array store in temp variable
+                    $given_array[$i] = $given_array[$j]; // value of inner array store in outer array index
+                    $given_array[$j] = $temp; // inner array index value is replace by temp value
+                }
+            }
+        }
+        return $given_array;
     }
 }
